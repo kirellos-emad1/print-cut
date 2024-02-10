1f0f7e7b04c507850f8731dad5bbb2b4ba6170b4
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-02-10 07:44:30 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,28 @@ import { CardWrapper } from "@/components/auth/CardWrapper";
 import { getAllProducts } from "@/data-access/products";
 import Image from "next/image";
 export default async function Home() {
-  const products = await getAllProducts()
+  const products = await getAllProducts();
   return (
     <main className="flex gap-x-4 m-5">
-      {products?.map((product)=>
+      {products?.map((product) => (
         <CardWrapper
-        headerLabel={product.title}
-        backButtonLabel="Discover"
-        backButtonHref={`/product/?product_id=${product.id}`}
-        key={product.id}
+          headerLabel={product.title}
+          backButtonLabel="Discover"
+          backButtonHref={`/product/?product_id=${product.id}`}
+          key={product.id}
         >
           <div className="flex flex-col items-center justify-center">
-          <Image src={product.images[0]} width={200} height={200} priority alt="product img" />
-          <p className="bg-black/30 p-1 rounded-xl w-[100px] text-white text-center">{product.price_by_meter} EGP</p>
-          <p>{product.summary}....</p>
+            <Image
+              src={product.images[0]}
+              width={200}
+              height={200}
+              priority
+              alt="product img"
+            />
+            <p className="bg-black/30 p-1 rounded-xl w-[100px] text-white text-center">
+              {product.price_by_meter} EGP
+            </p>
+            <p>{product.summary}....</p>
           </div>
         </CardWrapper>
       ))}
