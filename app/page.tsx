4c187e90583a450dd02bb0684b2bf957359ed197
Kirellos Wasfy <94471342+kirellos-emad1@users.noreply.github.com>
2024-02-10 07:35:02 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ export default async function Home() {
           <Image src={product.images[0]} width={200} height={200} priority alt="product img" />
           <p className="bg-black/30 p-1 rounded-xl w-[100px] text-white text-center">{product.price_by_meter} EGP</p>
           <p>{product.summary}....</p>
-          <p>{JSON.stringify(product.images)}</p>
           </div>
         </CardWrapper>
       ))}
